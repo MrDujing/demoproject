43526b709d670dea4ccf9a2c544ef888fc265702
bob <zhangbaochn@outlook.com>
2025-03-11 19:24:12 +0800

    change

---
+++ aliyun-nacos-demo/src/main/java/com/example/aliyunnacosdemo/AliyunNacosDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.aliyunnacosdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyunNacosDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunNacosDemoApplication.class, args);
+    }
+
+}
---
+++ aliyun-nacos-demo/src/test/java/com/example/aliyunnacosdemo/AliyunNacosDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aliyunnacosdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyunNacosDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
